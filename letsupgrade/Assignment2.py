26aeeca3747a4f867497e07c744dae5435064669
neerav34 <113015607+neerav34@users.noreply.github.com>
2023-07-02 01:34:37 +0530

    Update Assignment2.py

--- letsupgrade/Assignment2.py
+++ letsupgrade/Assignment2.py
@@ -1,6 +1,5 @@
 def vowelR(my_string):
     replacements = [('a', ''), ('e', ''), ('i', ''), ('o', ''), ('u', ''), ('A', ''), ('E', ''), ('I', ''), ('O', ''), ('U', '')]
-    print(my_string)
     for char, replacement in replacements:
         if char in my_string:
             my_string = my_string.replace(char, replacement)
